ba32a29246e55cc21dc418b89d1fab8b2a29e328
Igor@Akhmadeev <raigor@mail.ru>
2020-03-10 00:27:08 +0700

    script full b1.0

--- log_analyzer.py
+++ log_analyzer.py
@@ -7,6 +7,7 @@ import gzip
 import collections
 import operator
 import string
+import logging
 
 # url regular expression pattern
 MASK_URL = r'(?<=GET\s)(/\S+)'
@@ -30,7 +31,8 @@ Usage: python %s [--config] [file]
 config = {
     "REPORT_SIZE": 1000,
     "REPORT_DIR": "./reports",
-    "LOG_DIR": "./log"
+    "LOG_DIR": "./log",
+    "LOG_ANALYZER_PATH": None
 }
 
 template_report = "./report.html"
@@ -47,7 +49,7 @@ def search_last_log(config: dict) -> str:
     try:
         files = os.listdir(config["LOG_DIR"])
     except Exception as error_message:
-        print(error_message)
+        logging.exception('Module "search_last_log": %s', error_message)
     finally:
         if files != []:
             for index in range(0, len(files)):
@@ -77,20 +79,19 @@ def report_processing_check(config: dict, last_log: str) -> bool:
     try:
         files = os.listdir(config["REPORT_DIR"])
     except Exception as error_message:
-        print(error_message)
-    finally:
-        res = []
-        mask_date = re.findall(r'\d+', last_log)
-        mask_rep_last = r'' + mask_date[0][:4] + '.' + mask_date[0][4:6] + '.' + mask_date[0][6:]
-        for index in range(0, len(files)):
-            if re.fullmatch(mask_rep, files[index]):
-                res.append(files[index])
-        flag_processing_check = False
-        for index in res:
-            if re.search(mask_rep_last, index):
-                flag_processing_check += True
-            else:
-                flag_processing_check += False
+        logging.exception('Module "report_processing_check": %s', error_message)
+    res = []
+    mask_date = re.findall(r'\d+', last_log)
+    mask_rep_last = r'' + mask_date[0][:4] + '.' + mask_date[0][4:6] + '.' + mask_date[0][6:]
+    for index in range(0, len(files)):
+        if re.fullmatch(mask_rep, files[index]):
+            res.append(files[index])
+    flag_processing_check = False
+    for index in res:
+        if re.search(mask_rep_last, index):
+            flag_processing_check += True
+        else:
+            flag_processing_check += False
     return bool(flag_processing_check)
 
 
@@ -104,7 +105,7 @@ def parsing_string(string_pars: str, template: list) -> list:
     """
     pars_list = []
     for temp in template:
-        parsed_result = re.search(temp, string_pars);
+        parsed_result = re.search(temp, string_pars)
         if parsed_result is not None:
             pars_list.append(parsed_result.group())
         else:
@@ -120,14 +121,26 @@ def parsing_string_log(config: dict, log_file_name: str) -> list:
     :return: structure list [url:str, request_time:str]
     """
     log_file_path = config["LOG_DIR"] + '/' + log_file_name
-    if log_file_path.endswith(".gz"):
-        log_file = gzip.open(log_file_path, 'rt')
-    else:
-        log_file = open(log_file_path, 'rt', encoding='utf-8')
+    global total
+    global processed
+    try:
+        if log_file_path.endswith(".gz"):
+            log_file = gzip.open(log_file_path, 'rt')
+        else:
+            log_file = open(log_file_path, 'rt', encoding='utf-8')
+    except Exception as er:
+        logging.exception('Error %s', er)
+    total_str = 0
+    processed_str = 0
     for log_string in log_file:
+        total_str += 1
         parsed_list = parsing_string(log_string, [MASK_URL, MASK_REQUEST_TIME])
+        if parsed_list[0] != '':
+            processed_str += 1
         yield parsed_list
     log_file.close()
+    processed = processed_str
+    total = total_str
 
 
 def parsing_log(config: dict, log_file_name: str) -> list:
@@ -145,6 +158,11 @@ def parsing_log(config: dict, log_file_name: str) -> list:
 
 
 def sort_list_url(mass_url: list) -> dict:
+    """
+    Function of sorting the request time by the same URL
+    :param mass_url: list [ "url","time_request"]
+    :return: dictionary with elements 'url': [time_request1,time_request2,..]
+    """
     mas_sort_url = collections.defaultdict(list)
     for url, value_time in mass_url:
         mas_sort_url[url].append(value_time)
@@ -152,6 +170,11 @@ def sort_list_url(mass_url: list) -> dict:
 
 
 def count_list_item(list_item: list) -> int:
+    """
+    Calculate the number of items in the list for this URL
+    :param list_item: list of strings of numeric values
+    :return: number of items in the list
+    """
     return len(list_item)
 
 
@@ -159,22 +182,33 @@ def time_sum_url(list_item: list) -> float:
     """
     Amount calculation all item in list for this url, if not calculation gives 'None'
     :param list_item: list of strings of numeric values
-    :return: Sum of all numerical values in list for this url
+    :return: sum of all numerical values in list for this url
     """
     sum_item = 0
     try:
         for item in list_item:
             sum_item += float(item)
     except:
-        sum_item = 0#none
+        logging.exception('Error conversions in type "float" modul time_sum_url')
+        sum_item = 0  # none
     return sum_item
 
 
 def time_average(list_item: list) -> float:
+    """
+    The function determines the average values request time in list for a given URL
+    :param list_item: list with values request time
+    :return: the average request time
+    """
     return time_sum_url(list_item) / len(list_item)
 
 
 def time_max(list_item: list) -> float:
+    """
+    The function determines the maximum request time in list for a given URL
+    :param list_item: list with values request time
+    :return: the maximum request time
+    """
     max_value_item = 0
     for item_time in list_item:
         if float(item_time) > max_value_item:
@@ -193,6 +227,11 @@ def value_percent(value_item: float, value_all: float) -> float:
 
 
 def count_total_request(data_mas: dict) -> int:
+    """
+    Function counts the total number of queries in the "data_mas"
+    :param data_mas: dictionary {'url1': [time_request1, time_request2, ...], 'url2':...}
+    :return: total number of queries
+    """
     count_request = 0
     for index in data_mas.keys():
         count_request += len(data_mas[index])
@@ -200,6 +239,11 @@ def count_total_request(data_mas: dict) -> int:
 
 
 def time_total_request(data_mas: dict) -> float:
+    """
+    The function counts the total request time in the "data_mas".
+    :param data_mas: dictionary {'url': [time_request1, time_request2, ...]}
+    :return: total time request
+    """
     count_request_time = 0
     for index in data_mas.keys():
         for time_str in data_mas[index]:
@@ -208,6 +252,11 @@ def time_total_request(data_mas: dict) -> float:
 
 
 def median_time_request(list_item: list) -> float:
+    """
+    Function to find the median value of a list
+    :param list_item: given list (int, float) value [7,4,3,2,5,6,1]
+    :return: median value of the list [4]
+    """
     list_item_float = []
     for item in list_item:
         list_item_float.append(float(item))
@@ -216,6 +265,14 @@ def median_time_request(list_item: list) -> float:
 
 
 def write_url_dict(url_str: str, line_time: list, total_count: int, total_time: float) -> dict:
+    """
+    The function forms a string of of static parameters parameters for a given URL in the form of a dictionary
+    :param url_str: name URL
+    :param line_time: list values time_request for a given URL
+    :param total_count: total number of requests
+    :param total_time:  total time of requests
+    :return: dictionary of of statistical parameters for a given URL
+    """
     count_r = len(line_time)
     time_sum = time_sum_url(line_time)
     url_dict_stat = {"count": count_r,
@@ -230,40 +287,78 @@ def write_url_dict(url_str: str, line_time: list, total_count: int, total_time: 
 
 
 def create_result_mas(data_mas: dict) -> list:
+    """
+    Create function sorted by "max_time" list of statistics data
+    :param data_mas : source dictionary of url and time request
+    :return: list dictionary of url and request statistics
+    """
     total_count = count_total_request(data_mas)
     total_time = time_total_request(data_mas)
     result_mas = []
     for item_mas in data_mas.keys():
         result_mas.append(write_url_dict(item_mas, data_mas[item_mas], total_count, total_time))
-    result_mas_sort = sorted(result_mas, key = operator.itemgetter("time_sum"), reverse = True)
+    result_mas_sort = sorted(result_mas, key=operator.itemgetter("time_sum"), reverse=True)
     return result_mas_sort
 
 
-def create_report(config: dict, log_file_name: str , result_mas_sort: list):
+def create_report(conf: dict, log_file_name: str, result_mas_sort: list):
+    """
+    The function of creating a report file in the form of a table
+    :param conf: dictionary with structure containing 
+                   the directory log file 'REPORT_DIR'
+                   and report sample size 'REPORT_SIZE'
+    :param log_file_name: name of the log file for which
+                          the report is generated
+    :param result_mas_sort: sorted list of statistics [{},{},..]
+    :return: 
+    """
     try:
         file_template_report = open(template_report, 'r')
         template_text = file_template_report.read()
-    except:
-        pass
+    except Exception as er:
+        logging.exception('Error load template report: %s', er)
     finally:
         file_template_report.close()
-    if config["REPORT_SIZE"] < len(result_mas_sort):
-        size_rep = config["REPORT_SIZE"]
+    if conf["REPORT_SIZE"] < len(result_mas_sort):
+        size_rep = conf["REPORT_SIZE"]
     else:
         size_rep = len(result_mas_sort)
-
-
     report_text = string.Template(template_text)
-    report_text_file = report_text.safe_substitute(table_json = result_mas_sort[0:size_rep])
+    report_text_file = report_text.safe_substitute(table_json=result_mas_sort[0:size_rep])
     mask_date = re.findall(r'\d+', log_file_name)
-    name_rep_file = config['REPORT_DIR']+'/report-' + mask_date[0][:4] + '.' + mask_date[0][4:6] + '.' + mask_date[0][6:] + '.html'
+    name_rep_file = conf['REPORT_DIR'] + '/report-' + mask_date[0][:4] + '.' \
+                    + mask_date[0][4:6] + '.' + mask_date[0][6:] + '.html'
     try:
-        file_report = open(name_rep_file,'w', encoding='utf-8')
+        file_report = open(name_rep_file, 'w', encoding='utf-8')
         file_report.write(report_text_file)
-    except:
-        pass
+    except Exception as er:
+        logging.exception('Error create report %s: %s', name_rep_file, er)
     finally:
         file_report.close()
+
+
+def init_logging(conf: dict):
+    """
+    Logging module settings function
+    :param conf: dictionary with structure containing
+                    name log file 'LOG_ANALYZER_PATH',
+                    default 'LOG_ANALYZER_PATH' = None
+    :return:
+    """
+    format_log = '[%(asctime)s] %(levelname).1s %(message)s'
+    logging.basicConfig(
+        filename=conf["LOG_ANALYZER_PATH"],
+        format=format_log,
+        datefmt='%Y.%m.%d %H:%M:%S',
+        level=logging.INFO)
+
+
+def process_message(total_str: int, proccesed_str: int) -> str:
+    err = 100 - 100 * proccesed_str / total_str
+    if err < 60:
+        return 'Process complete'
+    else:
+        return 'Unable to parse most of the log Error >60% perhaps the logging format has changed'
 
 
 def main():
@@ -293,12 +388,17 @@ def main():
                             config[key] = config_from_file[key]
         else:
             sys.exit(error_message_comnd_line % sys.argv[0])
-    print('OK')
+    init_logging(config)
     log_name = search_last_log(config)
     if report_processing_check(config, log_name) == False:
+        logging.info('Last raw log found: %s', log_name)
         mass_passed_data = parsing_log(config, log_name)
         mass_passed_data_sort = sort_list_url(mass_passed_data)
         create_report(config, log_name, create_result_mas(mass_passed_data_sort))
+        logging.info(process_message(total,processed))
+    else:
+        logging.info('Last log has already been processed')
+
 
 if __name__ == "__main__":
     main()
